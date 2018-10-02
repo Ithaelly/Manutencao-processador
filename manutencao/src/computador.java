6621c538a945bd012ae3a24a2650c0f0b82779d0
ithaelly <ithaellymykarlla@gmail.com>
2018-10-02 20:49:49 -0300

    colocado o código todo em um laço principal

--- manutencao/src/computador.java
+++ manutencao/src/computador.java
@@ -16,67 +16,70 @@ public class computador {
                 System.out.println("1- mostrar os registradores a cada passo");
                 System.out.println("2- mostrar os registradores a cada grande grupo, como: buscar, decodificar");
                 System.out.println("3- mostrar os registradores a cada instrução, como: soma 5; carr 10;");
+                System.out.println("  ");
+                System.out.println("Para sair...aperte 'S' ");
                  Scanner ler = new Scanner(System.in);
                 System.out.println("  ");
                 p.r.setEscolha(ler.nextInt());
                 
+                  while(p.r.getEscolha() != 'S'){
+                            p.busca();//MAR=pc
+                            if(p.r.getEscolha()==2){
+                                System.out.println("entrou em Busca");
+                                System.out.print(p.r.imprimirtudo());
+                                System.out.println("  Memória:"+ mem.getMem());
+                                System.out.println("  ");
+                            }
 
-                p.busca();//MAR=pc
-                if(p.r.getEscolha()==2){
-                    System.out.println("entrou em Busca");
-                    System.out.print(p.r.imprimirtudo());
-                    System.out.println("  Memória:"+ mem.getMem());
-                    System.out.println("  ");
-                }
-                
-                mem.setMem(p.r.getMAR());//passa o "mar" como a posição da memória 
-                p.r.setMBR(mem.getMem());//pega o valor da "memória" nesta posição, e coloca dentro de "mbr"    
-                
-                p.busca2();//coloca "mbr" dentro de "ir"
-                if(p.r.getEscolha()==2){
-                    System.out.println("entrou em Busca2");
-                    System.out.print(p.r.imprimirtudo());
-                    System.out.println("  Memória:"+ mem.getMem());  
-                    System.out.println("  ");
-                }
-                
-                p.decodifica();//dividir os dados do vetor
-                if(p.r.getEscolha()==2){
-                    System.out.println("entrou em Decodificar");
-                    System.out.print(p.r.imprimirtudo());
-                    System.out.println("  Memória:"+ mem.getMem()); 
-                    System.out.println("  ");
-                }
-                
-                p.buscarDados();//pega o endereco e colocar no mar
-                if(p.r.getEscolha()==2){
-                    System.out.println("entrou em Buscar Dados");
-                    System.out.print(p.r.imprimirtudo());
-                    System.out.println("  Memória:"+ mem.getMem()); 
-                    System.out.println("  ");
-                }
+                            mem.setMem(p.r.getMAR());//passa o "mar" como a posição da memória 
+                            p.r.setMBR(mem.getMem());//pega o valor da "memória" nesta posição, e coloca dentro de "mbr"    
 
-                mem.setMem(p.r.getMAR());//passa o "mar" como a posição da memória 
-                p.r.setMBR(mem.getMem());//pega o valor da "memória" nesta posição, e coloca dentro de "mbr"    
+                            p.busca2();//coloca "mbr" dentro de "ir"
+                            if(p.r.getEscolha()==2){
+                                System.out.println("entrou em Busca2");
+                                System.out.print(p.r.imprimirtudo());
+                                System.out.println("  Memória:"+ mem.getMem());  
+                                System.out.println("  ");
+                            }
 
-                p.executa();//tipo faz a soma se for somar
-                if(p.r.getEscolha()==2){
-                    System.out.println("entrou em Executar");
-                    System.out.print(p.r.imprimirtudo());
-                    System.out.println("  Memória:"+ mem.getMem());
-                    System.out.println("  ");
-                }
-                
-                if(p.uc.getComando().equals("Salvar")){
-                    mem.setMem(p.r.getMAR());//passa o "mar" como a posição da memória 
-                    p.r.setMBR(mem.getMem());//pega o valor de mbr e coloca dentro da memória
-                    
-                    if(p.r.getEscolha()==3) {
-                        System.out.println("entrou em Salvar"); 
-                        System.out.print(p.r.imprimirtudo());
-                        System.out.println("  Memória:"+ mem.getMem());   
-                        System.out.println("  ");
-                    }
-                 }
+                            p.decodifica();//dividir os dados do vetor
+                            if(p.r.getEscolha()==2){
+                                System.out.println("entrou em Decodificar");
+                                System.out.print(p.r.imprimirtudo());
+                                System.out.println("  Memória:"+ mem.getMem()); 
+                                System.out.println("  ");
+                            }
+
+                            p.buscarDados();//pega o endereco e colocar no mar
+                            if(p.r.getEscolha()==2){
+                                System.out.println("entrou em Buscar Dados");
+                                System.out.print(p.r.imprimirtudo());
+                                System.out.println("  Memória:"+ mem.getMem()); 
+                                System.out.println("  ");
+                            }
+
+                            mem.setMem(p.r.getMAR());//passa o "mar" como a posição da memória 
+                            p.r.setMBR(mem.getMem());//pega o valor da "memória" nesta posição, e coloca dentro de "mbr"    
+
+                            p.executa();//tipo faz a soma se for somar
+                            if(p.r.getEscolha()==2){
+                                System.out.println("entrou em Executar");
+                                System.out.print(p.r.imprimirtudo());
+                                System.out.println("  Memória:"+ mem.getMem());
+                                System.out.println("  ");
+                            }
+
+                            if(p.uc.getComando().equals("Salvar")){
+                                mem.setMem(p.r.getMAR());//passa o "mar" como a posição da memória 
+                                p.r.setMBR(mem.getMem());//pega o valor de mbr e coloca dentro da memória
+
+                                if(p.r.getEscolha()==3) {
+                                    System.out.println("entrou em Salvar"); 
+                                    System.out.print(p.r.imprimirtudo());
+                                    System.out.println("  Memória:"+ mem.getMem());   
+                                    System.out.println("  ");
+                                }
+                             }
+                   }
     }
 }
