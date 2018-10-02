1c53db8b9bd7b7db5de0380b14e532463b4d748b
ithaelly <ithaellymykarlla@gmail.com>
2018-10-02 05:12:44 -0300

    criado os if's para mostrar os registradores, caso o usuário escolha a opção 1; criada os "souts" para mostrar a informação de que passo está sendo executado

--- manutencao/src/unidadeDeControle.java
+++ manutencao/src/unidadeDeControle.java
@@ -7,6 +7,7 @@ public class unidadeDeControle{
     private String comando = null;
 
     registradores r = new registradores();
+    memoria mem= new memoria();
     
     public void setEndereco(int endereco){
         this.endereco = endereco;
@@ -23,27 +24,37 @@ public class unidadeDeControle{
     public String getComando(){
         return comando;
     }
-    
+ 
     public void atualizaRegistradores(registradores i){
        r=i;
     }
-
     
+    public void atualizaMemoria(memoria m){
+        mem=m;
+    }
     
     public void colocandoPcDentroDeMar(){ //pega o valor do "pc" e coloca dentro do "mar"
         r.setMAR(r.getPc());
         r.incremetaPC();
+        System.out.println("Colocando PC dentro de MAR");
+         if(r.getEscolha()==1) System.out.print(r.imprimirtudo());  System.out.println("  Memória:"+ mem.getMem()); System.out.println("  ");
     } 
     public void colocandoMbrDentroDeIr(){ //IR= MBR
         r.setIR(r.getMBR());
+        System.out.println("Colocando MBR dentro de IR");
+        if(r.getEscolha()==1) System.out.print(r.imprimirtudo());  System.out.println("  Memória:"+ mem.getMem()); System.out.println("  ");
     }
     public void dividirVetor(){
         comando=r.getIR();       
         String[] spl =  comando.split(" ");
         comando=spl[0];    
         endereco = Integer.valueOf(spl[1]);
+        System.out.println("Dividindo o vetor");
+        if(r.getEscolha()==1) System.out.print(r.imprimirtudo());  System.out.println("  Memória:"+ mem.getMem()); System.out.println("  ");
     }
     public void pegarEnderecoColocarNoMar(){
         r.setMAR(endereco);
+        System.out.println("Pega o endereco e coloca no MAR");
+        if(r.getEscolha()==1) System.out.print(r.imprimirtudo());  System.out.println("  Memória:"+ mem.getMem()); System.out.println("  ");
     }
 }
